2312931133f3011347171f163501230777bf1ceb
Felipe Borba <felipe.borba0109@gmail.com>
2023-02-07 20:47:03 -0300

    Day 4 of 100 days of swiftui

---
+++ Day-004.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+let username: String // remeber from mathematical let X = 1 with means X is constant value
+//print(username)
+username = "X"
+print(username)
+//username = "f"
+
+// Checkpoint 2
+
+let duplicates = ["bob", "Fred", "bob", "matheus", "vini", "tyrant"]
+print(duplicates.count)
+let unique = Set(duplicates)
+print(unique.count)
