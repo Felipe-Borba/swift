d5b7333b340099a4fdb64dafc05e6f0f4eb455cb
Felipe Borba <felipe.borba0109@gmail.com>
2023-02-26 21:18:34 -0300

    Day 23 of 100 days of swiftui

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Felipe Silva de Borba on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
