a1aa00af25cd517ee7379c5be37222bef056e3bf
Felipe Borba <felipe.borba0109@gmail.com>
2023-03-03 00:13:38 -0300

    Day 27 of 100 days of swiftui

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Felipe Silva de Borba on 01/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
