3c0ee16b6735817f4c7bc449a4c0420a3def7f67
Felipe Borba <felipe.borba0109@gmail.com>
2023-03-24 07:49:03 -0300

    Day 39 of 100 days of swiftui

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Felipe Silva de Borba on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
