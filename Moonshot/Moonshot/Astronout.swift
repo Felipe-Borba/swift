909d2925b978b938051b4d28e0f310514d318cbd
Felipe Borba <felipe.borba0109@gmail.com>
2023-03-25 11:57:44 -0300

    Day 40 of 100 days of swiftui

---
+++ Moonshot/Moonshot/Astronout.swift
@@ -0,0 +1,14 @@
+//
+//  Astronout.swift
+//  Moonshot
+//
+//  Created by Felipe Silva de Borba on 25/03/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
