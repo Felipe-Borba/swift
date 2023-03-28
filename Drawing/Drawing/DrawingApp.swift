c342834133a9fd95540dfc7047dd242a56bd2fe8
Felipe Borba <felipe.borba0109@gmail.com>
2023-03-28 08:27:36 -0300

    Day 43 of 100 days of swiftui

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Felipe Silva de Borba on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
