b5a807f28b9401e2ed2fb804a66102c6575084ed
Felipe Borba <felipe.borba0109@gmail.com>
2023-03-30 07:50:11 -0300

    Day 45 of 100 days of swiftui

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,102 +7,91 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    var petalOffset = -20.0
-    var petalWidth = 100.0
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: Double
+    
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
+        }
+
+        return a
+    }
     
     func path(in rect: CGRect) -> Path {
-        // The path that will hold all petals
+        let divisor = gcd(innerRadius, outerRadius)
+        let outerRadius = Double(self.outerRadius)
+        let innerRadius = Double(self.innerRadius)
+        let distance = Double(self.distance)
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * Double.pi * outerRadius / Double(divisor)) * amount
+
         var path = Path()
-        
-        // Count from 0 up to pi * 2, moving up pi / 8 each time
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            // rotate the petal by the current value of our loop
-            let rotation = CGAffineTransform(rotationAngle: number)
-            
-            // move the petal to be at the center of our view
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            
-            // create a path for this petal using our properties plus a fixed Y and height
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-            
-            // apply our rotation/position transformation to the petal
-            let rotatedPetal = originalPetal.applying(position)
-            
-            // add it to our main path
-            path.addPath(rotatedPetal)
+
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+
+            x += rect.width / 2
+            y += rect.height / 2
+
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
         }
-        
-        // now send the main path back
+
         return path
     }
 }
 
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
-
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: Double(value))
-                    .strokeBorder(
-                        LinearGradient(
-                            gradient: Gradient(colors: [
-                                color(for: value, brightness: 1),
-                                color(for: value, brightness: 0.5)
-                            ]),
-                            startPoint: .top,
-                            endPoint: .bottom
-                        ),
-                        lineWidth: 2
-                    )
-            }
-        }
-        .drawingGroup()
-    }
-
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(steps) + amount
-
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
-    }
-}
-
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
-    @State private var colorCycle = 0.0
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount = 1.0
+    @State private var hue = 0.6
 
     var body: some View {
-        /*
-        VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(.red, style: FillStyle(eoFill: true))
-            
-            Capsule()
-                .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
-                .frame(width: 300, height: 200)
-            
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
+        VStack(spacing: 0) {
+            Spacer()
 
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
-        }
-         */
-        VStack {
-            ColorCyclingCircle(amount: colorCycle)
+            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
                 .frame(width: 300, height: 300)
 
-            Slider(value: $colorCycle)
+            Spacer()
+
+            Group {
+                Text("Inner radius: \(Int(innerRadius))")
+                Slider(value: $innerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Outer radius: \(Int(outerRadius))")
+                Slider(value: $outerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Distance: \(Int(distance))")
+                Slider(value: $distance, in: 1...150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Amount: \(amount, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $amount)
+                    .padding([.horizontal, .bottom])
+
+                Text("Color")
+                Slider(value: $hue)
+                    .padding(.horizontal)
+            }
         }
     }
 }
