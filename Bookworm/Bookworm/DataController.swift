eba9b35b31b26b4a7854930460f7ed7761c929c0
Felipe Borba <felipe.borba0109@gmail.com>
2023-04-09 14:58:57 -0300

    Day 53 of 100 days of swiftui

---
+++ Bookworm/Bookworm/DataController.swift
@@ -0,0 +1,13 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Felipe Silva de Borba on 09/04/23.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+}
---
+++ Bookworm/Bookworm/lesson/CoreDataView.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataView.swift
+//  Bookworm
+//
+//  Created by Felipe Silva de Borba on 09/04/23.
+//
+
+import SwiftUI
+
+struct CoreDataView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct CoreDataView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoreDataView()
+    }
+}
