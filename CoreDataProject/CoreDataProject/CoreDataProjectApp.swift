1d0d6fe4e5063719423e4407a5d766f4cf33a580
Felipe Borba <felipe.borba0109@gmail.com>
2023-04-13 07:51:56 -0300

    Day 57 of 100 days of swiftui

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,19 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Felipe Silva de Borba on 13/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    @StateObject private var dataController = DataController()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
