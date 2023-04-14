da7dfcb587c2bb99a77d716386628bc3d603c0eb
Felipe Borba <felipe.borba0109@gmail.com>
2023-04-14 08:06:35 -0300

    Day 58 of 100 days of swiftui

---
+++ CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Felipe Silva de Borba on 14/04/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
