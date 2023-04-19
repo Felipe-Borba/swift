5b6d9e8ee90fc61c6d733937c17a87b26de9ae78
Felipe Borba <felipe.borba0109@gmail.com>
2023-04-19 07:15:41 -0300

    Day 63 of 100 days of swiftui

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,39 +5,26 @@
 //  Created by Felipe Silva de Borba on 18/04/23.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
+    @State private var showingImagePicker = false
 
     var body: some View {
         VStack {
-            Text("Hello, World!")
-                .blur(radius: blurAmount)
-                .frame(width: 300, height: 300)
-                .background(backgroundColor)
-                .onTapGesture {
-                    showingConfirmation = true
-                }
-            
-            Slider(value: $blurAmount, in: 0...20)
+            image?
+                .resizable()
+                .scaledToFit()
 
-            Button("Random Blur") {
-                blurAmount = Double.random(in: 0...20)
+            Button("Select Image") {
+               showingImagePicker = true
             }
         }
-        .onChange(of: blurAmount) { newValue in
-            print("New value is \(newValue)")
-        }
-        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-            Button("Red") { backgroundColor = .red }
-            Button("Green") { backgroundColor = .green }
-            Button("Blue") { backgroundColor = .blue }
-            Button("Cancel", role: .cancel) { }
-        } message: {
-            Text("Select a new color")
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker()
         }
     }
 }
