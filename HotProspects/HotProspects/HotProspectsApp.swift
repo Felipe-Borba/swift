a3e81e94f643e8fdb1dadb16ff98acbe594c6e5e
Felipe Borba <felipe.borba0109@gmail.com>
2023-05-09 07:14:31 -0300

    Day 79 of 100 days of swiftui

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Felipe Silva de Borba on 09/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
