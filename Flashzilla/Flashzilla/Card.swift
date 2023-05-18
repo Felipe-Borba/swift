a082f962c9e58b20f15442f4eb05bad78ba8ebbb
Felipe Borba <felipe.borba0109@gmail.com>
2023-05-18 07:19:44 -0300

    Day 88 of 100 days of swiftui

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Felipe Silva de Borba on 18/05/23.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played Din Djarin in The Mandalorian?", answer: "Pedro Pascal")
+}
