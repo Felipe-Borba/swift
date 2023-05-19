16b62a7347a85fdf647edfcf54550b09eec28831
Felipe Borba <felipe.borba0109@gmail.com>
2023-05-19 07:40:25 -0300

    Day 89 of 100 days of swiftui

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -8,23 +8,36 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
     
     let card: Card
     var removal: (() -> Void)? = nil
-
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(.white)
+                .fill(
+                    differentiateWithoutColor
+                    ? .white
+                    : .white
+                        .opacity(1 - Double(abs(offset.width / 50)))
+                    
+                )
+                .background(
+                    differentiateWithoutColor
+                    ? nil
+                    : RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
             
             VStack {
                 Text(card.prompt)
                     .font(.largeTitle)
                     .foregroundColor(.black)
-
+                
                 if isShowingAnswer {
                     Text(card.answer)
                         .font(.title)
