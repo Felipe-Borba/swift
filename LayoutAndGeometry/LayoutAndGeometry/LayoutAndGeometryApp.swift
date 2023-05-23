6c6529c567e38caf0fd0b081f817b6b8209cae59
Felipe Borba <felipe.borba0109@gmail.com>
2023-05-23 07:02:19 -0300

    Day 92 of 100 days of swiftui

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Felipe Silva de Borba on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
